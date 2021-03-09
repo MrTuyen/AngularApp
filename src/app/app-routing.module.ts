b2c4ac751af5cb8a9a7fbbf6fc3a5857df39e397
Nguyen <Tuyen.Nguyen@hanes.com>
2021-03-09 16:32:46 +0700

    Register, forgot pass

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent}
+	{ path: 'login', component: LoginComponent },
+	{ path: 'forgot-password', component: ForgetPasswordComponent },
+	{ path: 'register', component: RegisterComponent }
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
